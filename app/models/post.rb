10b6d966169320a54a8478860a7a3d4785df9a2e
jinook929 <jinook929@gmail.com>
2021-03-18 08:45:34 -0400

    commit tree 8519b716bef0ea9018dc0f42f9520944d7677f02

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
 class Post < ActiveRecord::Base
-  
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+
+  validates :name, presence: true
+  validates :content, presence: true
+
+  # accepts_nested_attributes_for :tags
+
+  def tags_attributes=(tags_attributes)
+    tags_attributes.values.each {|tag_attributes|
+      if tag_attributes[:name].present?
+        tag = Tag.find_or_create_by(tag_attributes)
+        self.tags << tag
+      end
+    }
+  end
 end
